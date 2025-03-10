8233d37629d94ec4cd138fa5fde2d12a384164d9
abdesslemhamani <abdesslemhamani@gmail.com>
2025-03-10 17:22:56 +0100

    to be continuer

---
+++ main.js
@@ -0,0 +1,30 @@
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
+
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
+function createNoteEl(id, content) {
+    const element = document.createElement("textarea");
+    element.classList.add("note");
+    element.placeholder = "Empty Note";
+    element.value = content;
+  
+    element.addEventListener("dblclick", () => {
+        const warning = confirm("Do you want to delete this note?");
+        if (warning) {
+          deleteNote(id, element);
+        }
+      });
+      element.addEventListener("input", () => {
+        updateNote(id, element.value);
+      });
+    
+      return element;
+    }
+    function deleteNote(id, element) {
+        const notes = getNotes().filter((note)=>note.id != id)
+        saveNote(notes)
+        appEl.removeChild(element)
+    }
